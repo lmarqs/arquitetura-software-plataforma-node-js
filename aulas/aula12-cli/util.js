c4e3706f30b99f8894796eee2e9c0586bdfefc81
lmarqs <marques.analista@gmail.com>
2020-01-19 20:03:57 -0300

    refact dir structure

--- aulas/aula12-cli/util.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const logFileName = 'log.txt'
-
-const log = (content) => {
-  const currentDate = new Date()
-  fs.appendFileSync(logFileName, `${currentDate} -> ${content}\n`)
-}
-
-module.exports = log
