9727bf097cea33753ccd9e40d65f76d1990136ac
lmarqs <marques.analista@gmail.com>
2020-01-19 21:14:45 -0300

    add view engine

--- src/modulo04/app.js
+++ src/modulo04/app.js
@@ -8,6 +8,7 @@ var usersRouter = require('./routes/users')
 
 var app = express()
 
+app.set('view engine', 'ejs')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -15,6 +16,10 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/echo', (req, res) => {
+  res.render('echo.ejs', { term: req.query.t })
+})
+
 app.use('/users', usersRouter)
 
 module.exports = app
